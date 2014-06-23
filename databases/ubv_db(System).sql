40ba95e0f5f0b9219e415c71f2332d42401f8ced
Elibo <elijose.c@gmail.com>
2014-06-23 12:46:52 -0430

    Cambios DB del Sistema

--- databases/ubv_db(System).sql
+++ databases/ubv_db(System).sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50614
 File Encoding         : 65001
 
-Date: 2014-06-17 22:44:17
+Date: 2014-06-23 12:46:07
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,15 +24,15 @@ CREATE TABLE `aldea` (
   `nombre` varchar(100) NOT NULL,
   `direccion` text NOT NULL,
   `cod_municipio` int(11) NOT NULL,
+  `cedula_coordinador` int(11) NOT NULL,
   PRIMARY KEY (`cod_aldea`),
-  KEY `aldea_municipio` (`cod_municipio`),
-  CONSTRAINT `aldea_municipio` FOREIGN KEY (`cod_municipio`) REFERENCES `municipio` (`cod_municipio`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  KEY `aldea_municipio` (`cod_municipio`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of aldea
 -- ----------------------------
-INSERT INTO `aldea` VALUES ('1', 'Aldea Universitaria Cunaguaro', 'POr ahi en algun beta', '7');
+INSERT INTO `aldea` VALUES ('1', 'Aldea Universitaria Cunaguaro', 'Valencia', '7', '0');
 
 -- ----------------------------
 -- Table structure for `cohorte`
@@ -65,26 +65,7 @@ CREATE TABLE `configuracion` (
 -- ----------------------------
 -- Records of configuracion
 -- ----------------------------
-INSERT INTO `configuracion` VALUES ('1', 'Sistema UBV', 'carlos@ubv.com');
-
--- ----------------------------
--- Table structure for `coordinador_aldea`
--- ----------------------------
-DROP TABLE IF EXISTS `coordinador_aldea`;
-CREATE TABLE `coordinador_aldea` (
-  `cod_coordinador` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `cedula` int(11) NOT NULL,
-  `cod_aldea` int(11) NOT NULL,
-  PRIMARY KEY (`cod_coordinador`),
-  KEY `coordinador_persona` (`cedula`),
-  KEY `coordinador_aldea` (`cod_aldea`),
-  CONSTRAINT `coordinador_aldea` FOREIGN KEY (`cod_aldea`) REFERENCES `aldea` (`cod_aldea`),
-  CONSTRAINT `coordinador_persona` FOREIGN KEY (`cedula`) REFERENCES `persona` (`cedula`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of coordinador_aldea
--- ----------------------------
+INSERT INTO `configuracion` VALUES ('1', 'Sistema(UBV). Eje Central.', 'admin@ubv.com.ve');
 
 -- ----------------------------
 -- Table structure for `documentos`
@@ -103,8 +84,7 @@ CREATE TABLE `documentos` (
   `fondo_negro` tinyint(1) DEFAULT '0',
   `autenticidad_titulo` tinyint(1) DEFAULT '0',
   `notas_bachillerato` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`matricula`),
-  CONSTRAINT `documentos_estudiante` FOREIGN KEY (`matricula`) REFERENCES `estudiante` (`matricula`)
+  PRIMARY KEY (`matricula`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
@@ -124,16 +104,14 @@ CREATE TABLE `empleado` (
   `eliminado` int(1) DEFAULT '0',
   PRIMARY KEY (`cod_empleado`),
   KEY `empleado_persona` (`cedula`),
-  KEY `empleado_nivel` (`cod_nivel`),
-  CONSTRAINT `empleado_nivel` FOREIGN KEY (`cod_nivel`) REFERENCES `nivel` (`cod_nivel`),
-  CONSTRAINT `empleado_persona` FOREIGN KEY (`cedula`) REFERENCES `persona` (`cedula`)
+  KEY `empleado_nivel` (`cod_nivel`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of empleado
 -- ----------------------------
 INSERT INTO `empleado` VALUES ('1', '17681201', 'admin', '21232f297a57a5a743894a0e4a801fc3', '0', '0');
-INSERT INTO `empleado` VALUES ('2', '158889623', 'prueba', '21232f297a57a5a743894a0e4a801fc3', '0', '0');
+INSERT INTO `empleado` VALUES ('2', '15999040', 'prueba', '21232f297a57a5a743894a0e4a801fc3', '2', '0');
 
 -- ----------------------------
 -- Table structure for `entidad_federal`
@@ -185,21 +163,12 @@ CREATE TABLE `estudiante` (
   PRIMARY KEY (`matricula`),
   KEY `estudiante_persona` (`cedula`),
   KEY `estudiante_mencion` (`cod_pfg`),
-  KEY `estudiante_cohorte` (`cod_cohorte`),
-  CONSTRAINT `estudiante_cohorte` FOREIGN KEY (`cod_cohorte`) REFERENCES `cohorte` (`cod_cohorte`),
-  CONSTRAINT `estudiante_mencion` FOREIGN KEY (`cod_pfg`) REFERENCES `pfg` (`cod_pfg`),
-  CONSTRAINT `estudiante_persona` FOREIGN KEY (`cedula`) REFERENCES `persona` (`cedula`)
+  KEY `estudiante_cohorte` (`cod_cohorte`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of estudiante
 -- ----------------------------
-INSERT INTO `estudiante` VALUES ('1806040752', '18054822', '2', '1');
-INSERT INTO `estudiante` VALUES ('1806040843', '15210364', '1', '1');
-INSERT INTO `estudiante` VALUES ('1806041136', '85965412', '1', '1');
-INSERT INTO `estudiante` VALUES ('1806041219', '21040999', '1', '1');
-INSERT INTO `estudiante` VALUES ('1806050528', '11954827', '1', '1');
-INSERT INTO `estudiante` VALUES ('1806050551', '11258999', '1', '1');
 
 -- ----------------------------
 -- Table structure for `modulo`
@@ -218,7 +187,7 @@ CREATE TABLE `modulo` (
 -- Records of modulo
 -- ----------------------------
 INSERT INTO `modulo` VALUES ('config', 'Configuracion del sistema', 'configuracion.png', '1', '8');
-INSERT INTO `modulo` VALUES ('constancy', 'Constancia de Culminacion', 'culminacion.png', '1', '5');
+INSERT INTO `modulo` VALUES ('requests-constancy', 'Constancia de Culminacion', 'culminacion.png', '1', '5');
 INSERT INTO `modulo` VALUES ('documents', 'Consignacion de Recaudos', 'documentos.png', '1', '6');
 INSERT INTO `modulo` VALUES ('employees', 'Usuarios del Sistema', null, '0', '0');
 INSERT INTO `modulo` VALUES ('home', 'Inicio', null, '0', '1');
@@ -237,8 +206,7 @@ CREATE TABLE `municipio` (
   `cod_entidad_federal` int(3) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   PRIMARY KEY (`cod_municipio`),
-  KEY `municipio_entidad_federal` (`cod_entidad_federal`),
-  CONSTRAINT `municipio_entidad_federal` FOREIGN KEY (`cod_entidad_federal`) REFERENCES `entidad_federal` (`cod_entidad_federal`)
+  KEY `municipio_entidad_federal` (`cod_entidad_federal`)
 ) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
@@ -273,7 +241,7 @@ CREATE TABLE `nivel` (
 -- ----------------------------
 -- Records of nivel
 -- ----------------------------
-INSERT INTO `nivel` VALUES ('0', 'Administrador', 'home,config,students,requests-notes,requests-transfer,universities,reports,constancy,employees,documents');
+INSERT INTO `nivel` VALUES ('0', 'Administrador', 'home,config,students,requests-notes,requests-transfer,requests-constancy,universities,reports,employees,documents');
 INSERT INTO `nivel` VALUES ('1', 'Analista', 'home');
 INSERT INTO `nivel` VALUES ('2', 'Coordinador de Aldea', 'home,students,reports');
 
@@ -295,14 +263,8 @@ CREATE TABLE `persona` (
 -- ----------------------------
 -- Records of persona
 -- ----------------------------
-INSERT INTO `persona` VALUES ('11258999', 'Carlos', 'Lopez', '', '', '', 'ninguna');
-INSERT INTO `persona` VALUES ('11954827', 'Sinforozo', 'Petro', '', '', '', 'ninguna');
-INSERT INTO `persona` VALUES ('15210364', 'Manuel', 'Ramirez', '', '', '', 'ninguna');
 INSERT INTO `persona` VALUES ('17681201', 'Eli', 'Chavez', '0414-4720780', 'elijose.c@gmail.com', 'Trigal Norte', 'ninguna');
-INSERT INTO `persona` VALUES ('18054822', 'LLuvia', 'DNMC', '0424-4954451', 'nohely28@hotmail.com', 'Nirgua. Edo Yaracuy', 'ninguna');
-INSERT INTO `persona` VALUES ('21040999', 'Antonio', 'Ortega', '', '', '', 'ninguna');
-INSERT INTO `persona` VALUES ('85965412', 'Aristoteles', 'Chonfin', '', '', 'Naguanagua', 'ninguna');
-INSERT INTO `persona` VALUES ('158889623', 'Prueba', 'Prueba', '', '', '', 'ninguna');
+INSERT INTO `persona` VALUES ('15999040', 'Prueba', 'Prueba', '', '', '', 'ninguna');
 
 -- ----------------------------
 -- Table structure for `pfg`
@@ -314,15 +276,14 @@ CREATE TABLE `pfg` (
   `descripcion` text,
   `cod_aldea` int(4) DEFAULT NULL,
   PRIMARY KEY (`cod_pfg`),
-  KEY `pfg_aldea` (`cod_aldea`),
-  CONSTRAINT `pfg_aldea` FOREIGN KEY (`cod_aldea`) REFERENCES `aldea` (`cod_aldea`)
+  KEY `pfg_aldea` (`cod_aldea`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of pfg
 -- ----------------------------
-INSERT INTO `pfg` VALUES ('1', 'Informatica', 'Todo sobre la informatica', '1');
-INSERT INTO `pfg` VALUES ('2', 'Agronomia', 'No lo se', '1');
+INSERT INTO `pfg` VALUES ('1', 'Informatica', 'Ing Informatica', '1');
+INSERT INTO `pfg` VALUES ('2', 'Agronomia', 'Tecnico Superior Agronomo', '1');
 
 -- ----------------------------
 -- Table structure for `solicitud`
@@ -340,11 +301,9 @@ CREATE TABLE `solicitud` (
   `aldea_nueva` int(11) DEFAULT NULL,
   `comentarios` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `solicitud_estudiante_idx` (`matricula`),
-  CONSTRAINT `solicitud_estudiante` FOREIGN KEY (`matricula`) REFERENCES `estudiante` (`matricula`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  KEY `solicitud_estudiante_idx` (`matricula`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of solicitud
 -- ----------------------------
-INSERT INTO `solicitud` VALUES ('1', 'traslado', '1806040752', '2014-06-18 04:09:54', null, '0', null, null, null, null);
